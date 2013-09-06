f0b085b3ff7f136d9f0e41a3d5150b6dd4d81a04
Amar Palsapure <apalsapure@appacitive.com>
2013-09-06 13:32:45 +0530

    Updated the configuration
    added basic validation and more keys

--- server/shared/configuration.js
+++ server/shared/configuration.js
@@ -1,8 +1,15 @@
 exports.load = function () {
-   return {
+   var config = {
         apikey: 'YOUR API KEY', 
         env: 'sandbox', 
         appId: 'APPLICATION ID',
-        brand: 'YOUR BRAND NAME'
+        brand: 'YOUR BRAND NAME',
+        host: '',
+        upvotepts: 10,
+        downvotepts: 5,
+        answerpts: 20
    };
+   if(config.host == '') throw Error('Set the host in config');
+
+   return config;
 };
