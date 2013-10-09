e7faf8acbcf27c0eb2a177b3de009c2631246428
Amar Palsapure <apalsapure@appacitive.com>
2013-10-09 17:40:29 +0530

    dynamic responses were also getting minified. undone.

--- app.js
+++ app.js
@@ -61,6 +61,7 @@ function noCacheRequest() {
 		res.header("Cache-Control", "no-cache, no-store, must-revalidate");
 		res.header("Pragma", "no-cache");
 		res.header("Expires", 0);
+		res._no_minify = true;
 		next();
 	};
 };
